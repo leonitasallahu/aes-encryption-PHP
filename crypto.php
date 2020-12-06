06fc1617222b0f27de8d770b8c6c97f0195e4e85
Alma Fazliu <almafazliu@hotmail.com>
2020-12-06 20:40:51 +0100

    Add Decrypt function

--- crypto.php
+++ crypto.php
@@ -30,6 +30,41 @@ class Crypto {
 		return $ciphertext64;
 	}
 	
+	public static function decrypt($msg, $secret) {
+		if (!$msg) {
+			return "";
+		}
+
+		// Separate payload from potential hmac
+		$separated = explode(":", trim($msg));
+
+		// Extract HMAC if signed
+		$hmac = (isset($separated[1])) ? $separated[1] : null;
+
+		// Convert data-string to array
+		$data = self::base64_url_decode($separated[0]);
+
+		// Extract IV
+		$iv = substr($data, 0, self::$block_size);
+
+		// Extract Salt
+		$salt = substr($data, self::$block_size, self::$block_size);
+
+		// Extract ciphertext
+		$ciphertext = substr($data, self::$block_size * 2);
+
+		// Generate Key
+		$key = hash_pbkdf2('sha1', $secret, $salt, self::$iterations, self::$key_size, true);
+
+		// Ensure integrity if signed
+		if ($hmac && !hash_equals(self::sign($separated[0], $key), $hmac)) {
+			return "";
+		}
+
+		// Decrypt
+		return openssl_decrypt($ciphertext, self::$encryption_method, $key, OPENSSL_RAW_DATA, $iv);
+	}
+	
 	private static function base64_url_encode($str) {
 		return strtr(base64_encode($str), '+/', '-_');
 	}
